950d18bf4f6a3b503ab5a19ab38eb746c424b3a4
aliev.saftar94@gmail.com <56716472+Saftar94@users.noreply.github.com>
2021-07-07 00:41:31 +0300

    Add JS

--- src/modal.js
+++ src/modal.js
@@ -1,15 +1,13 @@
-
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
-    }
-  })();
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileBtnClose = document.querySelector("[data-menu-close]");
+
+  menuBtnRef.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+  });
+
+  mobileBtnClose.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
